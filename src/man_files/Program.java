ef89f381cfd7fa498753b5c426cdba045547ad25
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-01-12 15:14:32 -0300

    exercicios 12/1

---
+++ src/man_files/Program.java
@@ -0,0 +1,20 @@
+package man_files;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+        String[] lines = new String[] { "Good morning", "Good afternoon", "Good night" };
+        String path = "C:\\gubee_codes\\out.txt";
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
